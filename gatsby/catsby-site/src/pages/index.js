7abb804f015e7f9d709a4d0d0332248bef73d6db
Jeff Sutherland <jeffdsuth@gmail.com>
2020-08-27 12:57:30 -0700

    Initial page

--- gatsby/catsby-site/src/pages/index.js
+++ gatsby/catsby-site/src/pages/index.js
@@ -1,22 +1,40 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <h1>Hi peeps 👋</h1>
+    <ul>
+      {data.allRandomUser.edges.map(({ node }) => (
+        <li>
+          <img src={node.picture.thumbnail} alt={node.name.first} />
+          {node.name.first} {node.name.last}
+        </li>
+      ))}
+    </ul>
   </Layout>
 )
+
+export const query = graphql`
+  query Users {
+    allRandomUser {
+      edges {
+        node {
+          id
+          name {
+            first
+            last
+          }
+          picture {
+            thumbnail
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
