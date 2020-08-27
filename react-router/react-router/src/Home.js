03fd4eda328cf8692b04a315811f998600cb8b88
Jeff Sutherland <jeffdsuth@gmail.com>
2020-08-27 15:27:19 -0700

    Add navigation

--- react-router/react-router/src/Home.js
+++ react-router/react-router/src/Home.js
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom'
 
-const Home = () => 'Hello World';
+const Home = () => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState('')
+
+  return (
+    <>
+      <Link to="user/react-router">Go to the React Router user</Link>
+      <form onSubmit={e => {
+        e.preventDefault();
+        navigate(`/user/${user}`);
+      }}
+      >
+        <input
+          placeholder='input a username'
+          value={user}
+          onChange={e => setUser(e.target.value)}
+        />
+        <button disabled={!user}>Go to that user</button>
+      </form>
+    </>
+  )
+};
 
 export default Home;
