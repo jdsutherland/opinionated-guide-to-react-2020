b98634ff9043286d7a4b773bf8a7877cecb5bd6b
Jeff Sutherland <jeffdsuth@gmail.com>
2020-08-28 14:55:48 -0700

    Show styling single element
    Avoids adding a whole component

--- styling/styled-components/src/index.js
+++ styling/styled-components/src/index.js
@@ -1,7 +1,20 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import { ThemeProvider } from "styled-components";
+import styled, { ThemeProvider, css } from "styled-components/macro";
 import { theme, Style } from "./utils/styles";
+
+const Heading = styled.h2`
+  background: black;
+  padding: 8px 12px;
+  color: ${(props) => props.theme.colors.white};
+
+  ${({ react, theme }) =>
+    react &&
+    css`
+      background: ${theme.colors.white};
+      color: black;
+    `}
+`;
 
 function App() {
   const [word, setWord] = useState("");
@@ -11,8 +24,15 @@ function App() {
         <Style />
         <div className="App">
           <h1>Write "React"</h1>
-          <input value={word} onChange={(e) => setWord(e.target.value)} />
-          <h2 react={word.toLowerCase() === "react"}>{word}</h2>
+          <input
+            css={{
+              border: 'none',
+              padding: 8,
+            }}
+            value={word}
+            onChange={(e) => setWord(e.target.value)}
+          />
+          <Heading react={word.toLowerCase() === "react"}>{word}</Heading>
         </div>
       </>
     </ThemeProvider>
