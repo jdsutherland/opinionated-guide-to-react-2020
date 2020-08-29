f27f0c15388240a6152c6658fc4f774c871db369
Jeff Sutherland <jeffdsuth@gmail.com>
2020-08-29 16:00:17 -0700

    Add tab example

--- ui-toolkits/reakit/src/index.js
+++ ui-toolkits/reakit/src/index.js
@@ -1,16 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { useDialogState, Dialog, DialogDisclosure } from "reakit/Dialog";
+import { useTabState, Tab, TabList, TabPanel } from "reakit/Tab";
 
 export default function App() {
   const dialog = useDialogState();
-
+  const tab = useTabState();
   return (
     <>
       <DialogDisclosure {...dialog}>Open dialog</DialogDisclosure>
       <Dialog {...dialog} aria-label="Welcome Modal">
         I am a modal
       </Dialog>
+      <TabList {...tab} aria-label="My tabs">
+        <Tab {...tab} stopId="tab1">
+          Tab 1
+        </Tab>
+        <Tab {...tab} stopId="tab2">
+          Tab 2
+        </Tab>
+        <Tab {...tab} stopId="tab3">
+          Tab 3
+        </Tab>
+      </TabList>
+      <TabPanel {...tab} stopId="tab1">
+        Tab 1
+      </TabPanel>
+      <TabPanel {...tab} stopId="tab2">
+        Tab 2
+      </TabPanel>
+      <TabPanel {...tab} stopId="tab3">
+        Tab 3
+      </TabPanel>
     </>
   );
 }
