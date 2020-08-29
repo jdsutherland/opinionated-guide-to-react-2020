7a36d2fcaa3613bcecf4fc1fd6f03e8f3f212f1e
Jeff Sutherland <jeffdsuth@gmail.com>
2020-08-29 07:28:00 -0700

    Add items using useMutation

--- graphql/src/App.js
+++ graphql/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useQuery } from '@apollo/react-hooks';
+import React, { useState } from 'react';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 import { gql } from "apollo-boost";
 
 const PEOPLE = gql`
@@ -11,17 +11,47 @@ const PEOPLE = gql`
   }
 `;
 
+const ADD_PERSON = gql`
+  mutation newPerson($firstName: String) {
+    add(firstName: $firstName) {
+      id
+    }
+  }
+`;
+
 export default function App() {
   const { loading, data } = useQuery(PEOPLE);
+  const [AddPersonMutation, { loading: loadingMutation }] = useMutation(ADD_PERSON);
+  const [name, setName] = useState('')
+
+  const addPerson = (e) => {
+    e.preventDefault();
+    AddPersonMutation({
+      variables: {
+        firstName: name,
+      },
+    })
+    setName('');
+  };
+
   if (loading) return 'loading'
   return (
-    <>
+    <div>
+      <h1>Add a person</h1>
+      <form onSubmit={addPerson}>
+        <input
+          required
+          onChange={event => setName(event.target.value)}
+          value={name}
+        />
+        <button type="submit">{loadingMutation ? 'Loading' : 'Add'}</button>
+      </form>
       <h1>Random People</h1>
       <ul>
         {data.all.map(person => (
           <li>{person.firstName}</li>
         ))}
       </ul>
-    </>
+    </div>
   )
 }
