b3271a6f84fbfb2409d9d5725c4880ea09ffb2e6
Jeff Sutherland <jeffdsuth@gmail.com>
2020-08-30 12:06:55 -0700

    Use useCallback

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 const App = () => {
   const [num, setNum] = useState(0);
   const [plusOne, setPlusOne] = useState(0);
 
-  const add1 = (x) => {
+  const add1 = useCallback((x) => {
     console.log("here", x);
     return x + 1;
-  };
+  }, [])
 
   useEffect(() => {
     setPlusOne(add1(num));
