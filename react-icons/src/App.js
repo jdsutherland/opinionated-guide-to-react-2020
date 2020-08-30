65a892a2b6292c9ef5aca2b19ce0cd6063b380a3
Jeff Sutherland <jeffdsuth@gmail.com>
2020-08-30 06:51:51 -0700

    Initial react-icons

---
+++ react-icons/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { DiReact } from "react-icons/di";
+
+export default function App() {
+  return (
+    <div className="App">
+      <DiReact />
+    </div>
+  );
+}
