b2dac26d3d5369c6abe962a6959b118c31ef5a76
Jeff Sutherland <jeffdsuth@gmail.com>
2020-08-30 12:05:34 -0700

    useCallback initial

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,29 +1,22 @@
-import React, { useState, useMemo } from "react";
-
-function fibonacci(x) {
-  console.log("here");
-  if (x <= 0) return 0;
-  if (x === 1) return 1;
-  return fibonacci(x - 1) + fibonacci(x - 2);
-}
+import React, { useState, useEffect } from "react";
 
 const App = () => {
-  const [number, setNumber] = useState(10);
-  const [isGreen, setIsGreen] = useState(true);
-  const fib = useMemo(() => fibonacci(number), [number]);
+  const [num, setNum] = useState(0);
+  const [plusOne, setPlusOne] = useState(0);
 
+  const add1 = (x) => {
+    console.log("here", x);
+    return x + 1;
+  };
+
+  useEffect(() => {
+    setPlusOne(add1(num));
+  }, [num, add1]);
   return (
     <>
-      <button
-        onClick={() => setIsGreen(!isGreen)}
-        style={{ background: isGreen ? "#01FF70" : "#B10DC9" }}
-      >
-        useMemo Example
-      </button>
-      <h2>
-        Fibonacci of {number} is {fib}
-      </h2>
-      <button onClick={() => setNumber(number + 1)}>+</button>
+      <h2>{num}</h2>
+      <h3>{plusOne}</h3>
+      <button onClick={() => setNum(num + 1)}>+</button>
     </>
   );
 };
