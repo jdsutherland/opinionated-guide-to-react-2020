0361cf9bb73220536cd144d7d4d0e491c5b65dc5
Jeff Sutherland <jeffdsuth@gmail.com>
2020-08-30 07:22:37 -0700

    Style it

--- react-icons/src/App.js
+++ react-icons/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { DiReact } from "react-icons/di";
+import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <DiReact />
+      <DiReact size={100} color='#81dafb' className='react-icon' />
     </div>
   );
 }
